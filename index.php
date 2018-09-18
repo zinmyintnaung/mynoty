712234eb79b89c0a92be5635abace2e9169be0f9
zinmyintnaung <zinmyintnaung@gmail.com>
2018-09-18 17:05:35 +0800

    fetch update

--- index.php
+++ index.php
@@ -46,56 +46,56 @@
 <script>
 $(document).ready(function(){
  
- function load_unseen_notification(view = '')
- {
-  $.ajax({
-   url:"fetch.php",
-   method:"POST",
-   data:{view:view},
-   dataType:"json",
-   success:function(data)
-   {
-    $('.dropdown-menu').html(data.notification);
-    if(data.unseen_notification > 0)
+    function load_unseen_notification(view = '')
     {
-     $('.count').html(data.unseen_notification);
+        $.ajax({
+        url:"fetch.php",
+        method:"POST",
+        data:{view:view},
+        dataType:"json",
+        success:function(data)
+            {
+                //console.log(data.unseen_notification);
+                //console.log(data.notification);
+                $('.dropdown-menu').html(data.notification);
+                if(data.unseen_notification > 0)
+                {
+                    $('.count').html(data.unseen_notification);
+                }
+            }
+        });
     }
-   }
-  });
- }
  
- load_unseen_notification();
+    load_unseen_notification();
  
- $('#comment_form').on('submit', function(event){
-  event.preventDefault();
-  if($('#subject').val() != '' && $('#comment').val() != '')
-  {
-   var form_data = $(this).serialize();
-   $.ajax({
-    url:"insert.php",
-    method:"POST",
-    data:form_data,
-    success:function(data)
-    {
-     $('#comment_form')[0].reset();
-     load_unseen_notification();
-    }
-   });
-  }
-  else
-  {
-   alert("Both Fields are Required");
-  }
- });
+    $('#comment_form').on('submit', function(event){
+        event.preventDefault();
+        if($('#subject').val() != '' && $('#comment').val() != '')
+        {
+            var form_data = $(this).serialize();
+            $.ajax({
+                url:"insert.php",
+                method:"POST",
+                data:form_data,
+                success:function(data)
+                {
+                $('#comment_form')[0].reset();
+                load_unseen_notification();
+                }
+            });
+        }else{
+            alert("Both Fields are Required");
+        }
+    });
  
- $(document).on('click', '.dropdown-toggle', function(){
-  $('.count').html('');
-  load_unseen_notification('yes');
- });
+    $(document).on('click', '.dropdown-toggle', function(){
+        $('.count').html('');
+        load_unseen_notification('yes');
+    });
  
- setInterval(function(){ 
-  load_unseen_notification();; 
- }, 5000);
+    setInterval(function(){ 
+        load_unseen_notification();; 
+    }, 5000);
  
 });
 </script>
